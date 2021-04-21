d3c09fb4d4bd22df59b3a113950602677fe56f96
Elizabeth Romig <romigs2011@gmail.com>
2021-04-21 10:30:36 -0500

    Final working game

--- js/script.js
+++ js/script.js
@@ -7,13 +7,45 @@ const numRemaining = document.querySelector(".remaining span");
 const messages = document.querySelector(".message");
 const againBtn = document.querySelector(".play-again");
 
-const word = "magnolia";
-let guessedLetters = []
+let word = "magnolia";
+let guessedLetters = [];
+let numRemainingGuesses = 8;
+
+const getWord = async function() {
+    const request = await fetch( "https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const data = await request.text();
+    const wordsArray = data.split("\n");
+    const randomIndex = Math.floor(Math.random()*wordsArray.length);
+    word = wordsArray[randomIndex].trim();
+    console.log (word);
+    updateWord();
+}
+
+getWord();
+
+const countGuesses = function(guess){
+    const wordUpper = word.toUpperCase();
+    if (wordUpper.includes(guess)){
+        messages.innerText = `Yes! ${guess} is in the word.`
+    } else {
+        messages.innerText = `Nope! The word does not contain ${guess}.`;
+        numRemainingGuesses--;
+    }
+    console.log (numRemainingGuesses);
+    if (numRemainingGuesses === 0) {
+        remainingGuesses.innerText = `GAME OVER! You have no remaining guesses.  The word was ${wordUpper}.`
+    } else if (numRemainingGuesses === 1){
+        numRemaining.innerText = "only ONE"
+    } else {
+        numRemaining.innerText = `${numRemainingGuesses} guesses`;
+    }
+}
 
 const playerWon = function(){
     if (wordInProgress.innerText === word.toUpperCase()) {
         messages.classList.add("win");
         messages.innerHTML = '<p class="highlight">You guessed correct the word! Congrats!</p>';
+        remainingGuesses.innerText = `You only had ${numRemainingGuesses} missed letters.`
     }
 }
 
@@ -23,7 +55,7 @@ const updateWord = function(guessedLetters){
     //console.log ("Array: ", wordArray);
     let i=0;
     for (let letter of wordArray){
-        if (guessedLetters.includes(letter)) {
+        if (guessedLetters && guessedLetters.includes(letter)) {
             wordArray[i] = letter;
         } else {
             wordArray[i] = "\u25CF";
@@ -63,6 +95,7 @@ const validateInput = function (input){
 }
 
 const makeGuess = function(letter){
+    if (numRemainingGuesses > 0){
     letter = letter.toUpperCase();
     if (guessedLetters.includes(letter)) {
         messages.innerText = `You've already guessed ${letter}.  Try again!`;
@@ -70,7 +103,8 @@ const makeGuess = function(letter){
         guessedLetters.push(letter);
         updatePage();
         updateWord(guessedLetters);
-    }
+        countGuesses(letter);
+    }}
     //console.log (guessedLetters);
 }
 
